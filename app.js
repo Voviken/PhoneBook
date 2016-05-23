a26002368b1f5046ff801145c65730ee70db6702
yura <smhfanda@gmail.com>
2016-05-23 09:00:46 +0300

    Remove ui templates. Added and enabled cors package.

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
-var Client = require('node-rest-client').Client;
+var cors = require('cors');
 
-var index = require('./routes/index');
-var pb_api = require('./routes/pb_api');
-var phonebook = require('./routes/phonebook');
+var api = require('./routes/api');
 
 var app = express();
 
@@ -36,14 +34,9 @@ app.use(function(req, res, next) {
     });
 });
 
-app.use(function(req, res, next) {
-    req["client"] = new Client();
-    next();
-});
+app.use(cors());
 
-app.use('/', index);
-app.use('/pb_api', pb_api);
-app.use('/phonebook', phonebook);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
